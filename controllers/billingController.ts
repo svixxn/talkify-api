7669daf695d47567bfbfd12fe0e50559ee4ff6a5
Maksym Bakovetskyi <bakoveckiymax@gmail.com>
2025-06-07 00:23:24 +0300

    chore: check webhook availability

--- controllers/billingController.ts
+++ controllers/billingController.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express";
 import { handleWebhook } from "../utils/stripe";
-import { asyncWrapper } from "../utils/general";
+import { APIResponse, asyncWrapper } from "../utils/general";
+import { httpStatus } from "../utils/constants";
 
 export const handleBillingWebhook = asyncWrapper(
   async (req: Request, res: Response) => {
     try {
       const event = req.body;
 
-      console.log("Received webhook event:", event);
+      await handleWebhook(event);
 
-      const res = await handleWebhook(event);
+      return APIResponse(
+        res,
+        httpStatus.OK.code,
+        "Webhook received and processed successfully"
+      );
     } catch (error) {
       console.error("Error handling webhook:", error);
       return res.status(500).send("Internal Server Error");
